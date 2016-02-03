8c9dbc66e473a392826897b71d203fb42e93df67
rwheaton <ryan.wheaton@gmail.com>
2016-02-03 15:41:08 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
-default['teamcity']['version'] = '9.0.4'
+default['teamcity']['version'] = '9.1.6'
 default['teamcity']['service-name'] = 'teamcity-server'
 default['teamcity']['service-group'] = 'teamcity'
 default['teamcity']['service-username'] = 'teamcity'
 default['teamcity']['accept-license'] = true
 default['teamcity']['admin-username'] = 'teamcity'
 default['teamcity']['admin-password'] = 'teamcity'
-default['java']['jdk_version'] = 7
+default['java']['install_flavor'] = "oracle"
+default['java']['jdk_version'] = 8
+default['java']['oracle']['accept_oracle_download_terms'] = true
